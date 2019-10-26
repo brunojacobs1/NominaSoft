090fd33d45c1f72a294ea3af887abf897dfc383f
brunojacobs1 <brunojacobsschulz@gmail.com>
2019-10-26 18:46:24 -0500

    + PeriodoPago Activo spec

---
+++ src/NominaSoft.Core/Specifications/BusquedaPeriodoActivoSpecification.cs
@@ -0,0 +1,18 @@
+﻿using NominaSoft.Core.Entities;
+using NominaSoft.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NominaSoft.Core.Specifications
+{
+    public class BusquedaPeriodoActivoSpecification : BaseSpecification<PeriodoPago>
+    {
+        public BusquedaPeriodoActivoSpecification()
+            : base(pp => pp.Esactivo == true)
+        {
+            AddInclude(pp => pp.BoletasPago);
+            AddInclude(pp => pp.ConceptosPago);
+        }
+    }
+}
