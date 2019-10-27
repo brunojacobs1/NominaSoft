8d06eda52bdd18e50e5f912b422ef88ee68706c4
Miguel <duke_miguel99@hotmail.com>
2019-10-27 10:36:44 -0500

    Procesar Pagos avance

--- src/NominaSoft.UI/Controllers/ProcesarPagosController.cs
+++ src/NominaSoft.UI/Controllers/ProcesarPagosController.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NominaSoft.Core.Entities;
+using NominaSoft.Core.Interfaces;
+using NominaSoft.Core.Specifications;
+using NominaSoft.UI.ViewModels;
 
 namespace NominaSoft.UI.Controllers
 {
     public class ProcesarPagosController : Controller
     {
+        private readonly IRepository<PeriodoPago> _repositoryPeriodoPago;
+
+        public ProcesarPagosController(IRepository<PeriodoPago> repositoryPeriodoPago)
+        {
+            _repositoryPeriodoPago = repositoryPeriodoPago;
+        }
+        //public IActionResult ProcesarPagos()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
         public IActionResult ProcesarPagos()
         {
-            return View();
+            ViewModelProcesarPagos viewModelProcesarPagos = new ViewModelProcesarPagos();
+            viewModelProcesarPagos.PeriodoPago = _repositoryPeriodoPago.Get(new BusquedaPeriodoActivoSpecification());
+            return View(viewModelProcesarPagos);
         }
+
+        [HttpPost]
+        public ViewResult VerificarProcesado(ViewModelProcesarPagos viewModelProcesarPagos)
+        {
+
+            viewModelProcesarPagos.PeriodoPago = _repositoryPeriodoPago.Get(new BusquedaPeriodoActivoSpecification());
+            if(viewModelProcesarPagos.PeriodoPago != null)
+            {
+                viewModelProcesarPagos.PeriodoPago.BoletasPago = _repositoryPeriodoPago.get(new B);
+                foreach (BoletaPago BoletaPago in viewModelProcesarPagos.PeriodoPago.BoletasPago.ToList())
+                {
+                    if (!BoletaPago.Contrato.VerificarVigencia())
+                    {
+                        //necesito una lista de contratos
+                    }
+                }
+            }
+
+            return View("~/Views/ProcesarPagos/ProcesarPagos.cshtml",viewModelProcesarPagos);
+        }
+
     }
 }
---
+++ src/NominaSoft.UI/ViewModels/ViewModelProcesarPagos.cs
@@ -0,0 +1,14 @@
+﻿using NominaSoft.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NominaSoft.UI.ViewModels
+{
+    public class ViewModelProcesarPagos
+    {
+        public PeriodoPago PeriodoPago { get; set; }
+        public Empleado Empleado { get; set; }
+    }
+}
