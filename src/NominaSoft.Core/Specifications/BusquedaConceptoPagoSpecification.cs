50c92d7c54cb96be552a30d9790ff2f437d722dc
brunojacobs1 <brunojacobsschulz@gmail.com>
2019-10-28 11:33:26 -0500

    + BusquedaConceptoPagoSpec

---
+++ src/NominaSoft.Core/Specifications/BusquedaConceptoPagoSpecification.cs
@@ -0,0 +1,19 @@
+﻿using NominaSoft.Core.Entities;
+using NominaSoft.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NominaSoft.Core.Specifications
+{
+    public class BusquedaConceptoPagoSpecification : BaseSpecification<ConceptosDePago>
+    {
+        public BusquedaConceptoPagoSpecification(int idContrato, int idPeriodoPago)
+            : base(cp => cp.IdContrato == idContrato && cp.IdPeriodoPago == idPeriodoPago)
+        {
+            AddInclude(cp => cp.Contrato);
+            AddInclude(cp => cp.PeriodoPago);
+            AddInclude(cp => cp.BoletaPago);
+        }
+    }
+}
